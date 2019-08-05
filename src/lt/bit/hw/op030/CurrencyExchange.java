6ebccd418dda5f42817300845e721c3e85d5ce5e
Zilvinas <z.lasauskas@gmail.com>
2019-08-05 13:45:56 +0300

    from last to op033

--- src/lt/bit/hw/op030/CurrencyExchange.java
+++ src/lt/bit/hw/op030/CurrencyExchange.java
@@ -2,19 +2,62 @@ package lt.bit.hw.op030;
 
 public class CurrencyExchange {
 
-    private double euro;
-    private double dollar, pound, cad, yen, peso;
-//    private double currency;
+    private double euro, pintos, dollar, pound, cad, yen, peso;
 
     private static final double DOLLAR = 1.11045;
     private static final double POUND = 0.91273;
     private static final double CAD = 1.46564;
     private static final double YEN = 118.37;
     private static final double PESO = 21.4176;
+    private static final double DOLLAR1 = 1 / DOLLAR;
+    private static final double POUND1 = 1 / POUND;
+    private static final double CAD1 = 1 / CAD;
+    private static final double YEN1 = 1 / YEN;
+    private static final double PESO1 = 1 / PESO;
 
-//    public CurrencyExchange() {
-//        euro = 0;
-//    }
+    public <pintos> CurrencyExchange() {
+        pintos = 0;
+    }
+
+    public void setDollar1(double pintos) {
+        dollar = pintos * DOLLAR1;
+    }
+
+    public double getDollar1() {
+        return dollar;
+    }
+
+    public void setPound1(double pintos) {
+        pound = pintos * POUND1;
+    }
+
+    public double getPound1() {
+        return pound;
+    }
+
+    public void setCad1(double pintos) {
+        cad = pintos * CAD1;
+    }
+
+    public double getCad1() {
+        return cad;
+    }
+
+    public void setYen1(double pintos) {
+        yen = pintos * YEN1;
+    }
+
+    public double getYen1() {
+        return yen;
+    }
+
+    public void setPeso1(double pintos) {
+        peso = pintos * PESO1;
+    }
+
+    public double getPeso1() {
+        return peso;
+    }
 
     public CurrencyExchange(double euros) {
         euro = euros;
@@ -23,6 +66,7 @@ public class CurrencyExchange {
     public void setDollar(double euros) {
         dollar = euros * DOLLAR;
     }
+
     public double getDollar() {
         return dollar;
     }
@@ -30,6 +74,7 @@ public class CurrencyExchange {
     public void setPound(double euros) {
         pound = euros * POUND;
     }
+
     public double getPound() {
         return pound;
     }
@@ -37,6 +82,7 @@ public class CurrencyExchange {
     public void setCad(double euros) {
         cad = euros * CAD;
     }
+
     public double getCad() {
         return cad;
     }
@@ -44,6 +90,7 @@ public class CurrencyExchange {
     public void setYen(double euros) {
         yen = euros * YEN;
     }
+
     public double getYen() {
         return yen;
     }
@@ -51,11 +98,8 @@ public class CurrencyExchange {
     public void setPeso(double euros) {
         peso = euros * PESO;
     }
+
     public double getPeso() {
         return peso;
     }
-
-//    public double getCurrency() {
-//        return currency;
-//    }
 }
--- src/lt/bit/hw/op030/CurrencyExchangeClient.java
+++ src/lt/bit/hw/op030/CurrencyExchangeClient.java
@@ -6,38 +6,86 @@ public class CurrencyExchangeClient {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter an Euro amount: ");
-        double euros = scan.nextDouble();
 
-        System.out.println("Enter the currency to convert to (1) US dollar, (2) GB pound, (3) CD dol, " +
+        System.out.println("Enter the currency to convert from (0) Euro, (1) US dollar, (2) GB pound, (3) CD dol, " +
                 "(4) JP yen, (5) MX peso : ");
-        int response = scan.nextInt();
+        int response1 = scan.nextInt();
 
-        CurrencyExchange currencyExchange = new CurrencyExchange(euros);
+        System.out.print("Enter an amount: ");
+        double pintos = scan.nextDouble();
+        CurrencyExchange currencyExchange1 = new CurrencyExchange(pintos);
 
-        switch (response) {
+        double value = 0;
+        switch (response1) {
+            case 0:
+                value = pintos;
+//                System.out.println(pintos);
+                break;
             case 1:
-                currencyExchange.setDollar(euros);
-                System.out.println(currencyExchange.getDollar());
+                currencyExchange1.setDollar1(pintos);
+                value = currencyExchange1.getDollar1();
+//                System.out.println(currencyExchange1.getDollar1());
                 break;
             case 2:
-                currencyExchange.setPound(euros);
-                System.out.println(currencyExchange.getPound());
+                currencyExchange1.setPound1(pintos);
+                value = currencyExchange1.getPound1();
+//                System.out.println(currencyExchange1.getPound1());
                 break;
             case 3:
-                currencyExchange.setCad(euros);
-                System.out.println(currencyExchange.getCad());
+                currencyExchange1.setCad1(pintos);
+                value = currencyExchange1.getCad1();
+//                System.out.println(currencyExchange1.getCad1());
                 break;
             case 4:
-                currencyExchange.setYen(euros);
-                System.out.println(currencyExchange.getYen());
+                currencyExchange1.setYen1(pintos);
+                value = currencyExchange1.getYen1();
+//                System.out.println(currencyExchange1.getYen1());
                 break;
             case 5:
-                currencyExchange.setPeso(euros);
-                System.out.println(currencyExchange.getPeso());
+                currencyExchange1.setPeso1(pintos);
+                value = currencyExchange1.getPeso1();
+//                System.out.println(currencyExchange1.getPeso1());
                 break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + response1);
         }
-//        System.out.println(currencyExchange.getCurrency());
+
+        double euros;
+        euros = value;
+
+        System.out.println("Enter the currency to convert to (0) Euro, (1) US dollar, (2) GB pound, (3) CD dol, " +
+                "(4) JP yen, (5) MX peso : ");
+        int response2 = scan.nextInt();
+
+        CurrencyExchange currencyExchange2 = new CurrencyExchange(euros);
+
+        switch (response2) {
+            case 0:
+                System.out.printf("%.2f", euros);
+                break;
+            case 1:
+                currencyExchange2.setDollar(euros);
+                System.out.printf("%.2f",currencyExchange2.getDollar());
+                break;
+            case 2:
+                currencyExchange2.setPound(euros);
+                System.out.printf("%.2f", currencyExchange2.getPound());
+                break;
+            case 3:
+                currencyExchange2.setCad(euros);
+                System.out.printf("%.2f", currencyExchange2.getCad());
+                break;
+            case 4:
+                currencyExchange2.setYen(euros);
+                System.out.printf("%.2f", currencyExchange2.getYen());
+                break;
+            case 5:
+                currencyExchange2.setPeso(euros);
+                System.out.printf("%.2f", currencyExchange2.getPeso());
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + response2);
+        }
     }
 }
 
